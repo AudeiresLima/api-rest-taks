c99ce490b221bcbe27641050751dbfe49e5e661e
AudeiresLima <audeireslima1999@gmail.com>
2024-04-09 22:10:22 -0300

    Utilizando extensão vs code para teste

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.status(200).send('Oi mundo');
+const database = {
+    proximo_id : 2,
+    tasks: [
+    {
+        id: 1,
+        title: "Tarefa inicial",
+        description: "Faremos nossa primeira implmentação API REST"
+    }
+    ]
+}
+
+app.get('/tasks', (req, res)=>{
+    res.status(200).json(database.tasks);
 })
+
+app.post('/tasks', (req, res) => {
+    const tarefa = req.body;
+    tarefa.id = database.proximo_id;
+    database.proximo_id++;
+    console.log(tarefa);
+    database.tasks.push(tarefa);
+
+    res.status(201).send('tarefa criada', tarefa);
+
+});
+
+app.delete('/tasks/id:', (req, res) => {
+
+});
 
 app.listen(5555, ()=>{
     console.log('Executando servidor...')
